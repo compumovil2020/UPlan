ce8b91707495e6ab7335af8d0988cf96082488ff
Juan Castañeda <castaneda_juan@javeriana.edu.co>
2020-09-07 17:36:28 -0500

    Pantallas mejoradas y agregadas
    Mejorado inicio de sesion y main, agregado registro y feed

--- UPlan/app/src/main/java/com/example/uplan/Login.java
+++ UPlan/app/src/main/java/com/example/uplan/Login.java
@@ -1,20 +1,29 @@
 package com.example.uplan;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity {
 
-    private TextView mTextView;
+    private TextView registro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        mTextView = (TextView) findViewById(R.id.text);
+        registro = (TextView) findViewById(R.id.link_registro);
+        registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(),SignUp.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
--- UPlan/app/src/main/java/com/example/uplan/MainActivity.java
+++ UPlan/app/src/main/java/com/example/uplan/MainActivity.java
@@ -1,20 +1,30 @@
 package com.example.uplan;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextView;
+    private Button comencemos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextView = (TextView) findViewById(R.id.text);
+        comencemos = (Button) findViewById(R.id.comencemos);
+        comencemos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(),Login.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
