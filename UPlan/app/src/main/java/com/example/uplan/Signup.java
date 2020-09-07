230735db5ebcfa430508e6c869f12586a278e4d1
Juan Castañeda <castaneda_juan@javeriana.edu.co>
2020-09-07 16:52:43 -0500

    Pantallas mejoradas
    Mejoradas inicio, inicio de sesión. registrarse

--- UPlan/app/src/main/java/com/example/uplan/Signup.java
+++ UPlan/app/src/main/java/com/example/uplan/Signup.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class crearEvento extends AppCompatActivity {
+public class Signup extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crear_evento);
+        setContentView(R.layout.activity_signup);
     }
 }
