b8dbdfda72024a4e162b8dfb547292b385e7e6eb
Juan Castañeda <castaneda_juan@javeriana.edu.co>
2020-09-08 00:40:58 -0500

    Intents nuevos, botones agregados

--- UPlan/app/src/main/java/com/example/uplan/Login.java
+++ UPlan/app/src/main/java/com/example/uplan/Login.java
@@ -3,24 +3,34 @@ package com.example.uplan;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity {
 
-    private TextView registro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        registro = (TextView) findViewById(R.id.link_registro);
+        TextView registro = (TextView) findViewById(R.id.link_registro);
+        Button butlogin = (Button) findViewById(R.id.boton_login);
+
         registro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(view.getContext(),SignUp.class);
+                startActivity(intent);
+            }
+        });
+
+        butlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(),Feed.class);
                 startActivity(intent);
             }
         });
--- UPlan/app/src/main/java/com/example/uplan/SignUp.java
+++ UPlan/app/src/main/java/com/example/uplan/SignUp.java
@@ -2,13 +2,53 @@ package com.example.uplan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
 
 public class SignUp extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        Button facebook = (Button) findViewById(R.id.fb);
+        Button google = (Button) findViewById(R.id.google);
+        Button registrarse = (Button) findViewById(R.id.boton_registrarse);
+
+        registrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(),Feed.class);
+                startActivity(intent);
+            }
+        });
+
+        facebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                WebView web = new WebView(view.getContext());
+                setContentView(web);
+                web.setWebViewClient( new WebViewClient());
+                web.loadUrl("https://www.facebook.com");
+            }
+        });
+
+        google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                WebView web = new WebView(view.getContext());
+                setContentView(web);
+                web.setWebViewClient( new WebViewClient());
+                web.loadUrl("https://myaccount.google.com/");
+            }
+        });
+
+
     }
 }
