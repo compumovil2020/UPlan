09ea3e45830643e39aa21a0e513a6dd2b3dd3a00
Andrés Ruiz <andrewz2010@hotmail.com>
2020-09-08 00:27:37 -0500

    2 Pantallas Encuentros

---
+++ UPlan/app/src/main/java/com/example/uplan/EncuentrosActivity.java
@@ -0,0 +1,14 @@
+package com.example.uplan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EncuentrosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_encuentros);
+    }
+}
