b60b1f17d67f56484691643e750a496a19e7dda7
Diego-Gomez-code <diko2140@gmail.com>
2020-09-08 20:17:38 -0500

    cambios

--- UPlan/app/src/main/java/com/example/uplan/crearEvento.java
+++ UPlan/app/src/main/java/com/example/uplan/crearEvento.java
@@ -2,13 +2,41 @@ package com.example.uplan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class crearEvento extends AppCompatActivity {
+
+    Button perfil;
+    Button feed;
+    Button options;
+    Button encuentros;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_evento);
+    }
+
+    public void feed(View v){
+        Intent intent = new Intent(v.getContext(), Feed.class);
+        startActivity(intent);
+    }
+
+    public void opciones(View v){
+        //Intent intent = new Intent(v.getContext(), Opciones.class);
+        //startActivity(intent);
+    }
+
+    public void pefil(View v){
+        //Intent intent = new Intent(v.getContext(), perfil.class);
+        //startActivity(intent);
+    }
+
+    public void encuentros(View v){
+        Intent intent = new Intent(v.getContext(), EncuentrosActivity.class);
+        startActivity(intent);
     }
 }
