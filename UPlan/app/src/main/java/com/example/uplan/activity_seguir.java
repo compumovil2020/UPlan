454fe21c2791db2c73b1048ccd203ee02b07f082
Brayan González <bjgonzaleza@gmail.com>
2020-09-08 19:35:10 -0500

    intents y barra iconos

--- UPlan/app/src/main/java/com/example/uplan/activity_seguir.java
+++ UPlan/app/src/main/java/com/example/uplan/activity_seguir.java
@@ -2,13 +2,31 @@ package com.example.uplan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class activity_seguir extends AppCompatActivity {
-
+    Button seguir;
+    Button perfil;
+    Button opciones;
+    Button feed;
+    Button encuentros;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seguir);
+        encuentros = findViewById(R.id.encuentrosU);
+        feed = findViewById(R.id.feedU);
+        opciones = findViewById(R.id.optionsU);
+        perfil = findViewById(R.id.perfilU);
+        seguir = findViewById(R.id.unfollowUsuario);
+
+    }
+
+    public void seguirUnfollow (View v){
+      Intent intent = new Intent(this,activity_unfollow.class);
+      startActivity(intent);
     }
 }
--- UPlan/app/src/main/java/com/example/uplan/activity_unfollow.java
+++ UPlan/app/src/main/java/com/example/uplan/activity_unfollow.java
@@ -2,13 +2,29 @@ package com.example.uplan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class activity_unfollow extends AppCompatActivity {
-
+    Button unfollow;
+    Button perfil;
+    Button opciones;
+    Button feed;
+    Button encuentros;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_unfollow);
+        encuentros = findViewById(R.id.encuentrosU);
+        feed = findViewById(R.id.feedU);
+        opciones = findViewById(R.id.optionsU);
+        perfil = findViewById(R.id.perfilU);
+        unfollow = findViewById(R.id.unfollowUsuario);
+    }
+    public void unfollowSeguir (View v){
+        Intent intent = new Intent(this,activity_seguir.class);
+        startActivity(intent);
     }
 }
