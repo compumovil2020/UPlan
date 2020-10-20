828e3ce4e205a0c388fb0f3e7b5877bdb0f44fd9
Diego Alejandro Gomez Parra <diko2140@gmail.com>
2020-10-19 20:26:37 -0500

    Permisos de cámara y galería

--- UPlan/app/src/main/java/com/example/uplan/editarPerfil.java
+++ UPlan/app/src/main/java/com/example/uplan/editarPerfil.java
@@ -1,30 +1,51 @@
 package com.example.uplan;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.view.ViewCompat;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.ColorStateList;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class editarPerfil extends AppCompatActivity {
+    static final int IMAGE_PICKER_REQUEST = 0;
+    static final int IMAGE_PICKER_ID = 2;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int IMAGE_CAPTURE_ID = 3;
 
-    TextView anadirFoto,textView6,textView9;
+    TextView anadirFoto,textView6,textView9, anadirCamara;
     EditText editTextTextMultiline,textView8;
     Button button5,button6;
     ConstraintLayout layout;
+    ImageView profileImage;
 
     private SensorManager sensorManager;
     private Sensor lightSensor;
@@ -35,7 +56,8 @@ public class editarPerfil extends AppCompatActivity {
         setContentView(R.layout.activity_editar_peril);
 
         layout= findViewById(R.id.layoutEditarPerfil);
-        anadirFoto=findViewById(R.id.anadirFoto);
+        anadirFoto=findViewById(R.id.anadirFotoGaleria);
+        anadirCamara = findViewById(R.id.anadirFotoCamara);
         textView6=findViewById(R.id.textView6);
         textView9=findViewById(R.id.textView9);
         final ColorStateList colorViejo = textView6.getTextColors();
@@ -43,6 +65,7 @@ public class editarPerfil extends AppCompatActivity {
         textView8=findViewById(R.id.textView8);
         button5=findViewById(R.id.button5);
         button6=findViewById(R.id.button6);
+        profileImage = findViewById(R.id.profileImage);
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
@@ -55,6 +78,7 @@ public class editarPerfil extends AppCompatActivity {
                     anadirFoto.setTextColor(getResources().getColor(R.color.accentMorado));
                     textView6.setTextColor(getResources().getColor(R.color.blanco));
                     textView9.setTextColor(getResources().getColor(R.color.blanco));
+                    anadirCamara.setTextColor(getResources().getColor(R.color.accentMorado));
                     editTextTextMultiline.setHintTextColor(getResources().getColor(R.color.blanco));
                     editTextTextMultiline.setTextColor(getResources().getColor(R.color.blanco));
                     ViewCompat.setBackgroundTintList(editTextTextMultiline, ColorStateList.valueOf(Color.WHITE));
@@ -71,6 +95,8 @@ public class editarPerfil extends AppCompatActivity {
                     anadirFoto.setTextColor(getResources().getColor(R.color.morado));
                     textView6.setTextColor(colorViejo);
                     textView9.setTextColor(colorViejo);
+                    anadirCamara.setTextColor(colorViejo);
+                    anadirCamara.setTextColor(getResources().getColor(R.color.morado));
                     editTextTextMultiline.setHintTextColor(colorViejo);
                     editTextTextMultiline.setTextColor(colorViejo);
                     ViewCompat.setBackgroundTintList(editTextTextMultiline, colorViejo);
@@ -103,4 +129,74 @@ public class editarPerfil extends AppCompatActivity {
         this.finish();
     }
 
+    public void seleccionarimagen(View v){
+        solicitarPermiso(this, Manifest.permission.READ_EXTERNAL_STORAGE, "NECESITO", IMAGE_PICKER_ID);
+        selecImagen();
+    }
+    public void abrirCamara(View v){
+        solicitarPermiso(this, Manifest.permission.CAMERA, "necesito", IMAGE_CAPTURE_ID);
+        tomarFoto();
+    }
+    public void selecImagen(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+            Intent selecImagen = new Intent(Intent.ACTION_PICK);
+            selecImagen.setType("image/*");
+            startActivityForResult(selecImagen, IMAGE_PICKER_REQUEST);
+        }
+
+    }
+    public void tomarFoto(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    public void solicitarPermiso(Activity context, String permiso, String justificacion, int idPermiso){
+        if(ContextCompat.checkSelfPermission(context, permiso) != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(context, permiso)){
+                Toast.makeText(this, justificacion, Toast.LENGTH_SHORT).show();
+            }
+            ActivityCompat.requestPermissions(context, new String[]{permiso}, idPermiso);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case IMAGE_PICKER_ID:
+                selecImagen();
+                break;
+            case IMAGE_CAPTURE_ID:
+                tomarFoto();
+                break;
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case IMAGE_PICKER_REQUEST:
+                if(resultCode == RESULT_OK){
+                    try{
+                        final Uri imageUri = data.getData();
+                        final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                        final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                        profileImage.setImageBitmap(selectedImage);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
+            case REQUEST_IMAGE_CAPTURE:
+                if(resultCode == RESULT_OK){
+                    Bundle extras = data.getExtras();
+                    Bitmap imageBitmap = (Bitmap) extras.get("data");
+                    profileImage.setImageBitmap(imageBitmap);
+                }
+        }
+    }
 }
