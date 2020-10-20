fdd216c579f3fec1e058e621e66e72d007299a0f
Juan Castañeda <castaneda_juan@javeriana.edu.co>
2020-10-19 20:55:03 -0500

    LogIn implementado

--- UPlan/app/src/main/java/com/example/uplan/Login.java
+++ UPlan/app/src/main/java/com/example/uplan/Login.java
@@ -2,22 +2,39 @@ package com.example.uplan;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+    private EditText email;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mAuth = FirebaseAuth.getInstance();
         TextView registro = (TextView) findViewById(R.id.link_registro);
         Button butlogin = (Button) findViewById(R.id.boton_login);
+        email = (EditText) findViewById(R.id.userText);
+        password = (EditText) findViewById(R.id.passwordText);
 
         registro.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,10 +47,65 @@ public class Login extends AppCompatActivity {
         butlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), Navigation.class);
-                startActivity(intent);
+                signInUser(email.getText().toString(),password.getText().toString());
             }
         });
 
+    }
+
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+    private void updateUI(FirebaseUser currentUser){
+        if(currentUser!=null){
+            Intent intent = new Intent(getBaseContext(), Navigation.class);
+            intent.putExtra("user", currentUser.getEmail());
+            startActivity(intent);
+        } else {
+            email.setText("");
+            password.setText("");
+        }
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+        String user = email.getText().toString();
+        if (TextUtils.isEmpty(user)) {
+            email.setError("Required.");
+            valid = false;
+        } else {
+            email.setError(null);
+        }
+        String pass = password.getText().toString();
+        if (TextUtils.isEmpty(pass)) {
+            password.setError("Required.");
+            valid = false;
+        } else {
+            password.setError(null);
+        }
+        return valid;
+    }
+
+    private void signInUser(String email, String password) {
+        if (validateForm()) {
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Log.d("LOG", "signInWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                updateUI(user);
+                            } else {
+                                Log.w("LOG", "signInWithEmail:failure", task.getException());
+                                Toast.makeText(Login.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                                updateUI(null);
+                            }
+                        }
+                    });
+        }
     }
 }
--- UPlan/app/src/main/java/com/example/uplan/SignUp.java
+++ UPlan/app/src/main/java/com/example/uplan/SignUp.java
@@ -1,31 +1,51 @@
 package com.example.uplan;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class SignUp extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private EditText username, email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        username = (EditText) findViewById(R.id.userText);
+        password = (EditText) findViewById(R.id.passText);
+        email = (EditText) findViewById(R.id.mailText);
 
         Button facebook = (Button) findViewById(R.id.fb);
         Button google = (Button) findViewById(R.id.google);
         Button registrarse = (Button) findViewById(R.id.boton_registrarse);
+        mAuth = FirebaseAuth.getInstance();
 
         registrarse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), Navigation.class);
-                startActivity(intent);
+                registerUser(email.getText().toString(), password.getText().toString());
             }
         });
 
@@ -51,4 +71,43 @@ public class SignUp extends AppCompatActivity {
 
 
     }
+
+    private void registerUser(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Log.d("SIGN", "createUserWithEmail:onComplete:" + task.isSuccessful());
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            if(user!=null){ //Update user Info
+                                UserProfileChangeRequest.Builder upcrb = new UserProfileChangeRequest.Builder();
+                                upcrb.setDisplayName(username.getText().toString());
+                                upcrb.setPhotoUri(Uri.parse("path/to/pic"));//fake uri, use Firebase Storage
+                                user.updateProfile(upcrb.build());
+                                updateUI(user);
+                            }
+                        }
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(SignUp.this, "Auth failed"+ task.getException().toString(),
+                                    Toast.LENGTH_SHORT).show();
+                            Log.e("SIGN", task.getException().getMessage());
+                        }
+                    }
+                });
+
+    }
+
+    private void updateUI(FirebaseUser currentUser){
+        if(currentUser!=null){
+            Intent intent = new Intent(getBaseContext(), Navigation.class);
+            intent.putExtra("user", currentUser.getEmail());
+            startActivity(intent);
+        } else {
+            email.setText("");
+            password.setText("");
+            username.setText("");
+        }
+    }
+
 }
